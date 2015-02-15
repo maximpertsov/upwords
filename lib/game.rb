73eba335e7ba9703201cf3452d96d0ade4d241f1
Maxim Pertsov <maxim.pertsov@gmail.com>
2015-02-15 16:38:43 -0500

    Minor refactoring to Curses UI: removed all magic constants related to board representation

--- lib/game.rb
+++ lib/game.rb
@@ -96,43 +96,48 @@ module Upwords
       @win.close
     end
 
-    def draw_grid(dim,sz_y,sz_x,y,x)
+    def draw_grid(dim)
+      sz_x, sz_y = SPACE_SIZE_X, SPACE_SIZE_Y
       (0...dim).each do |j|
-        (0...dim).each do |i|
-          swin = draw_subwin(@win, sz_y, sz_x, y+j*(sz_y-1), x+i*(sz_x-1))
-          swin.box("|", "-")
-          # make corners '+' (plus-signs)
-          corners = [[0,0],[sz_y-1,0],[0,sz_x-1],[sz_y-1,sz_x-1]]
-          corners.each do |y,x|
-            swin.setpos(y,x)
-            swin.addch("+")
-          end
-        end
+        (0...dim).each {|i| draw_cell(sz_x, sz_y, i*(sz_x-1), j*(sz_y-1))}
       end
-      @win.setpos(y,x)
+      @win.setpos(CURSOR_START_Y, CURSOR_START_X)
     end
 
     # TODO: make this a private method ?
-    def draw_subwin(win, sz_y,sz_x,y,x)
-      win.subwin(sz_y,sz_x,y,x)    
+    def draw_cell(sz_x, sz_y, x, y)
+      cell = @win.subwin(sz_y, sz_x, y, x)
+      cell.box(VERTICAL_DIVIDER_CHR, HORIZONTAL_DIVIDER_CHR)
+      draw_corners(cell, CORNER_CHR, sz_x, sz_y)
+    end
+
+    def draw_corners(cell, char, sz_x, sz_y)
+      corners = [0, sz_x-1].product [0, sz_y-1]
+      corners.each do |x,y|
+        cell.setpos(y,x)
+        cell.addch(char)
+      end
     end
 
     def winloop
+      x_start, y_start = CURSOR_START_X, CURSOR_START_Y
+      x_step, y_step = SPACE_SIZE_X - 1, SPACE_SIZE_Y - 1
+      x_bound, y_bound = [x_step, y_step].map{|i| i * @board.side_length}
       @win.keypad(true)
       Curses.noecho
-      @win.setpos(1,2)
+      @win.setpos(y_start, x_start)
       @running = true
       while @running do
         key = @win.getch
         case key
         when Curses::KEY_LEFT
-          @win.setpos(@win.cury, @win.curx-5)
+          @win.setpos(@win.cury, [x_start, (@win.curx - x_step) % x_bound].max)
         when Curses::KEY_RIGHT
-          @win.setpos(@win.cury, @win.curx+5)
+          @win.setpos(@win.cury, [x_start, (@win.curx + x_step) % x_bound].max)
         when Curses::KEY_UP
-          @win.setpos(@win.cury-2, @win.curx)
+          @win.setpos([y_start, (@win.cury - y_step) % y_bound].max, @win.curx)
         when Curses::KEY_DOWN
-          @win.setpos(@win.cury+2, @win.curx)
+          @win.setpos([y_start, (@win.cury + y_step) % y_bound].max, @win.curx)
         when 27 # This corresponds to ESC or Alt+A
           @running = false
         else
@@ -142,10 +147,9 @@ module Upwords
             @win.setpos(@win.cury, @win.curx - cap_letter.size)
           end
         end
-        @win.refresh  
       end
     end
-
+    
     # =========================================
     # Game Loops & Non-Input Procedures
     # =========================================
@@ -154,12 +158,11 @@ module Upwords
       # new Curses window subroutine (STILL BEING IMPLEMENTED!)
       if @use_curses
         open_window
-        draw_grid(10,3,6,0,0)
+        draw_grid(@board.side_length)
         winloop
         close_window
       # old basic console subroutine
       else
-        ARGV.clear
         while @running do
           refresh_graphics
           begin
@@ -269,6 +272,7 @@ module Upwords
       'S' => [ 1, 0], # down
       'A' => [ 0,-1], # left 
       'D' => [ 0, 1]  # right
+      
     } 
     DIRECTION_KEYMAP.default = [0,0]
 
@@ -279,6 +283,19 @@ module Upwords
       '4' => proc { skip_turn },
       '5' => proc { exit_game }
     }
+
+    # =========================================
+    # Board Configurations
+    # =========================================
+
+    CURSOR_START_X = 2
+    CURSOR_START_Y = 1
+    SPACE_SIZE_X = 6
+    SPACE_SIZE_Y = 3
+
+    CORNER_CHR = "+"
+    HORIZONTAL_DIVIDER_CHR = "-"
+    VERTICAL_DIVIDER_CHR = "|"
 
   end
 end
