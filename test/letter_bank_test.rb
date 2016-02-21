c04f76decc45a2e5ecb8e333ad5618f0298260d1
Maxim Pertsov <maxim.pertsov@gmail.com>
2016-02-21 01:50:23 -0500

    Added LetterBank tests

---
+++ test/letter_bank_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class LetterBankTest < Minitest::Test
+  include Upwords
+
+  def setup
+    @bank = LetterBank.new(['A','B','C','D'])
+  end
+
+  def test_can_draw_letters
+    4.times do
+      assert_includes(['A','B','C','D'], @bank.draw)
+    end
+  end
+
+  def test_raises_error_on_draws_from_empty_bank
+    4.times { @bank.draw }
+    assert_raises(IllegalMove) { @bank.draw }
+  end
+
+  def test_empty?
+    refute @bank.empty?
+    
+    4.times { @bank.draw }
+
+    assert @bank.empty?
+  end
+
+  def test_can_deposit_new_letters
+    @bank.deposit('E')
+
+    5.times do
+      assert_includes(['A','B','C','D','E'], @bank.draw)
+    end
+
+    assert @bank.empty?
+  end
+end
