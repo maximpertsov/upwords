140f17349f451f0ce2105b9db6302c12d3200962
Maxim Pertsov <maxim.pertsov@gmail.com>
2016-04-05 18:43:20 -0400

    Added full board

--- lib/upwords/ui.rb
+++ lib/upwords/ui.rb
@@ -1,33 +1,36 @@
 module Upwords
   class UI
 
-    def self.loop(game)
+    def self.main_loop(game)
+      # Initialize Curses
       Curses.noecho
+      Curses.curs_set(2)
       Curses.init_screen
       
       begin
+        # Initialize window
         win = Curses.stdscr
         win.keypad=(true)
-        x = 0
-        y = 0
-        win.setpos(y, x)
+        win.setpos(0, 0)
         
-        # Draw cells
-        cell = win.subwin(3, 5, 0, 0)
-        (0..3).each do |x|
-          (0..3).each do |y|
-            win.subwin(3, 5, y * 3, x * 5).box('-', '-')
-          end
-        end
-
+        # Draw board in sub-window
+        blines = self.board_lines(10, 10, 4)
+        subwin = win.subwin(blines.length, blines[0].length + 1, 0, 0)
+        subwin.addstr(blines.join("\n"))
         win.refresh
         
-        # TODO: make this a while-loop
-        self.read_key(win, game)
-        win.getch
+        # Input loop
+        while (self.read_key(win, game)) do; end
+
       ensure
         Curses.close_screen
       end
+    end
+
+    def self.board_lines(rows, cols, col_width)
+      divider = [nil, ["-" * col_width] * cols, nil].flatten.join("+")
+      spaces = [nil, [" " * col_width] * cols, nil].flatten.join("|")
+      return ([divider] * (rows + 1)).zip([spaces] * rows).flatten
     end
 
     def self.read_key(win, game)
@@ -35,18 +38,16 @@ module Upwords
       when Curses::Key::UP
         win.setpos(win.cury - 1, win.curx)
       when Curses::Key::DOWN
-         win.setpos(win.cury + 1, win.curx)
+        win.setpos(win.cury + 1, win.curx)
       when Curses::Key::LEFT
         win.setpos(win.cury, win.curx - 1)
       when Curses::Key::RIGHT
-        win.setpos(win.cury, win.curx + 1) 
+        win.setpos(win.cury, win.curx + 1)
+      else
+        return false
       end
 
       return key
-    end
-
-    def self.draw_board(game)
-      # TODO: Implement...
     end
   end
 
