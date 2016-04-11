99f4d0c448919bc90055768ed0d2d40763d7fd03
Maxim Pertsov <maxim.pertsov@gmail.com>
2016-04-11 15:26:39 -0400

    Updated main_loop to OO-design

--- lib/upwords/ui.rb
+++ lib/upwords/ui.rb
@@ -1,33 +1,40 @@
 module Upwords
   class UI
 
-    def self.main_loop(game)
-      # Initialize Curses
+    def initialize(game)
+      @game = game
+
+      # Configure Curses and initialize screen
       Curses.noecho
       Curses.curs_set(2)
       Curses.init_screen
-      
+   
       begin
-        # Initialize window
-        win = Curses.stdscr
-        win.keypad=(true)
-        win.setpos(0, 0)
-        
-        # Draw board in sub-window
-        blines = self.board_lines(game.board, 4) 
-        subwin = win.subwin(blines.length, blines[0].length + 1, 0, 0)
-        subwin.addstr(blines.join("\n"))
-        win.refresh
-        
-        # Input loop
-        while (self.read_key(win, game)) do; end
-
+        @win = Curses.stdscr
+        @win.keypad=(true)
+        @win.setpos(0, 0) # TODO: Change this to top-left of middle square
+        main_loop
       ensure
         Curses.close_screen
       end
     end
 
-    def self.board_lines(board, col_width) 
+    def main_loop
+      # Draw board in sub-window
+      self.draw
+
+      # Input loop
+      while (self.read_key) do; end
+    end
+
+    def draw
+      blines = self.board_lines(@game.board, 4) 
+      subwin = @win.subwin(blines.length, blines[0].length + 1, 0, 0)
+      subwin.addstr(blines.join("\n"))
+      @win.refresh
+    end
+
+    def board_lines(board, col_width) 
       rows = board.num_rows
       cols = board.num_columns
 
@@ -37,23 +44,29 @@ module Upwords
       return ([divider] * (rows + 1)).zip([spaces] * rows).flatten
     end
 
-    def self.read_key(win, game)
-      case (key = win.getch)
+    def read_key
+      case (key = @win.getch)
       when Curses::Key::UP
-        game.cursor.up
+        @game.cursor.up
       when Curses::Key::DOWN
-        game.cursor.down
+        @game.cursor.down
       when Curses::Key::LEFT
-        game.cursor.left
+        @game.cursor.left
       when Curses::Key::RIGHT
-        game.cursor.right
+        @game.cursor.right
       else
         return false
       end
       
       # Update Curses cursor to new game cursor
-      win.setpos((game.cursor.y * 2) + 1, (game.cursor.x * 5) + 2)
+      @win.setpos((@game.cursor.y * 2) + 1, (@game.cursor.x * 5) + 2)
       return key
+    end
+
+    private
+
+    def win_pos(y, x)
+      [(y * 2) + 1, (x * 5) + 2]
     end
   end
 
