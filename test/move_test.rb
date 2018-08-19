5a55be3d0858a3fef1ecb8f547e537cfa87ef629
Maxim Pertsov <maxim.pertsov@gmail.com>
2018-08-18 23:18:27 -0400

    Rubocop move tests

--- test/move_test.rb
+++ test/move_test.rb
@@ -1,34 +1,35 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class MoveTest < Minitest::Test
   include Upwords
 
   def setup
-    @move = Move.new([[[0,0], 'c'], [[0,1], 'a'], [[0,2], 't']])
+    @move = Move.new([[[0, 0], 'c'], [[0, 1], 'a'], [[0, 2], 't']])
   end
 
   def test_check_if_position_exists
-    assert @move.position?(0,0)
-    refute @move.position?(1,1)
+    assert @move.position?(0, 0)
+    refute @move.position?(1, 1)
   end
 
   def test_get_letter
-    assert_equal 'c', @move[0,0]
-    assert_equal 'a', @move[0,1]
-    assert_equal 't', @move[0,2]
+    assert_equal 'c', @move[0, 0]
+    assert_equal 'a', @move[0, 1]
+    assert_equal 't', @move[0, 2]
   end
 
   # Helper function: created board from [letter, row, col] array
   def make_board(moves)
-    moves.reduce(Board.new(10, 5)) do |board, (letter, row, col)|
+    moves.each_with_object(Board.new(10, 5)) do |(letter, row, col), board|
       board.play_letter(letter, row, col)
-      board
     end
   end
 
   def test_can_play_to_board
     b = @move.play(Board.new(10, 5))
-    
+
     assert_equal 'c', b.top_letter(0, 0)
     assert_equal 'a', b.top_letter(0, 1)
     assert_equal 't', b.top_letter(0, 2)
@@ -45,28 +46,27 @@ class MoveTest < Minitest::Test
     b = Board.new(10, 5)
     b.play_letter('c', 0, 0)
     b.play_letter('a', 0, 1)
-    
-    assert_raises(IllegalMove) {@move.remove_from(b)}
+
+    assert_raises(IllegalMove) { @move.remove_from(b) }
   end
 
   def test_new_words
     b = make_board([['a', 1, 0], ['r', 2, 0], ['t', 3, 0]])
-  
-    new_words = @move.new_words(b).map{|w| w.to_s}
-    
+
+    new_words = @move.new_words(b).map(&:to_s)
+
     assert_equal 2, new_words.size
-    assert_includes(new_words, "cat")
-    assert_includes(new_words, "cart")
+    assert_includes(new_words, 'cat')
+    assert_includes(new_words, 'cart')
   end
 
   def test_illegal_words
     dict = Dictionary.new(['cart'])
     b = make_board([['a', 1, 0], ['r', 2, 0], ['t', 3, 0]])
-  
-    illegal_words = @move.new_illegal_words(b, dict).map {|w| w.to_s}
-    
-    assert_equal 1, illegal_words.size
-    assert_includes(illegal_words, "cat")
-  end
 
+    illegal_words = @move.new_illegal_words(b, dict).map(&:to_s)
+
+    assert_equal 1, illegal_words.size
+    assert_includes(illegal_words, 'cat')
+  end
 end
